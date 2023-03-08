70f340c457b63f78dba6de27f9c76ddc0a1670f0
nightshiftmaster <nightshiftmaster@Nights-MacBook-Air.local>
2023-03-09 01:33:15 +0200

    rollbar

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -2,17 +2,17 @@ import App from "./App";
 
 import store from "./slices";
 import { Provider as ReduxProvider } from "react-redux";
-import "./i18n";
 import { Provider, ErrorBoundary } from "@rollbar/react";
+import "./i18n";
 
-const rollbarConf = {
+const rollbarConfig = {
   accessToken: "f7631b7d3d50493083cb2438ac4c5681",
   environment: "production",
 };
 
 const init = async () => {
   return (
-    <Provider config={rollbarConf}>
+    <Provider config={rollbarConfig}>
       <ErrorBoundary>
         <ReduxProvider store={store}>
           <App />
