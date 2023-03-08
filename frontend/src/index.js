afb27c203bf0daac8d51fb6f6561c441c708d099
nightshiftmaster <nightshiftmaster@Nights-MacBook-Air.local>
2023-03-08 19:07:12 +0200

    rollbar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,11 @@ import React from "react";
 import "./index.css";
 import init from "./init";
 import { Provider, ErrorBoundary } from "@rollbar/react";
-import Rollbar from "rollbar";
 
-const rollbarConfig = new Rollbar({
+var rollbarConfig = {
   accessToken: "f7631b7d3d50493083cb2438ac4c5681",
   environment: "production",
-  captureUncaught: true,
-  captureUnhandledRejections: true,
-});
+};
 
 const app = async () => {
   const root = ReactDOM.createRoot(document.getElementById("root"));
