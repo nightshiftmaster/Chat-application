acc1e7aebda87e85b50cb480228979b100b134c6
nightshiftmaster <nightshiftmaster@Nights-MacBook-Air.local>
2023-03-08 17:51:35 +0200

    rollbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,38 +8,29 @@ import { Notfound } from "./pages/Notfoundpage";
 import RequireAuth from "./hooks/RequireAuth";
 import { AuthorizeProvider } from "./hooks/AuthorizeProvider";
 import { SignUp } from "./pages/Signuppage";
-import { Provider, ErrorBoundary } from "@rollbar/react";
 
 const App = () => {
-  const rollbarConfig = {
-    accessToken: "c25695d18a2e4454ae98432b59249a0e",
-    environment: "testenv",
-  };
   return (
-    <Provider config={rollbarConfig}>
-      <ErrorBoundary>
-        <AuthorizeProvider>
-          <BrowserRouter>
-            <div className="d-flex flex-column h-100">
-              <Navbar />
-              <Routes>
-                <Route path="login" element={<Login />} />
-                <Route path="signup" element={<SignUp />} />
-                <Route
-                  path="/"
-                  element={
-                    <RequireAuth>
-                      <Main />
-                    </RequireAuth>
-                  }
-                />
-                <Route path="*" element={<Notfound />} />
-              </Routes>
-            </div>
-          </BrowserRouter>
-        </AuthorizeProvider>
-      </ErrorBoundary>
-    </Provider>
+    <AuthorizeProvider>
+      <BrowserRouter>
+        <div className="d-flex flex-column h-100">
+          <Navbar />
+          <Routes>
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<SignUp />} />
+            <Route
+              path="/"
+              element={
+                <RequireAuth>
+                  <Main />
+                </RequireAuth>
+              }
+            />
+            <Route path="*" element={<Notfound />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </AuthorizeProvider>
   );
 };
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,24 @@
 import ReactDOM from "react-dom/client";
+import React from "react";
 import "./index.css";
 import init from "./init";
+import { Provider, ErrorBoundary } from "@rollbar/react";
+import Rollbar from "rollbar";
+
+const rollbarConfig = new Rollbar({
+  accessToken: "f7631b7d3d50493083cb2438ac4c5681",
+  environment: "production",
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
 
 const app = async () => {
   const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(await init());
+  root.render(
+    <Provider config={rollbarConfig}>
+      <ErrorBoundary>{await init()}</ErrorBoundary>
+    </Provider>
+  );
 };
 
 app();
