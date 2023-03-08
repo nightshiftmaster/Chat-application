1a977765e1ee08b611619604b1a48749cb8dcd12
nightshiftmaster <nightshiftmaster@Nights-MacBook-Air.local>
2023-03-09 00:05:35 +0200

    rollbar

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,13 +1,24 @@
 import App from "./App";
 
 import store from "./slices";
-import { Provider } from "react-redux";
+import { Provider as ReduxProvider } from "react-redux";
 import "./i18n";
+import { Provider, ErrorBoundary } from "@rollbar/react";
+
+const rollbarConfig = {
+  accessToken: "f7631b7d3d50493083cb2438ac4c5681",
+  environment: "production",
+};
 
 const init = async () => {
   return (
-    <Provider store={store}>
-      <App />
+    <Provider config={rollbarConfig}>
+      <ErrorBoundary>
+        {" "}
+        <ReduxProvider store={store}>
+          <App />
+        </ReduxProvider>{" "}
+      </ErrorBoundary>
     </Provider>
   );
 };
