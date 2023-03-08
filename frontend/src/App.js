d1934b20dfc2152245aa6b3b314eb344112f9fd6
nightshiftmaster <nightshiftmaster@Nights-MacBook-Air.local>
2023-03-08 02:25:59 +0200

    rollbar update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,35 +8,37 @@ import { Notfound } from "./pages/Notfoundpage";
 import RequireAuth from "./hooks/RequireAuth";
 import { AuthorizeProvider } from "./hooks/AuthorizeProvider";
 import { SignUp } from "./pages/Signuppage";
-import { Provider } from "@rollbar/react";
+import { Provider, ErrorBoundary } from "@rollbar/react";
 
 const App = () => {
   const rollbarConfig = {
-    accessToken: "POST_CLIENT_ITEM_ACCESS_TOKEN",
-    environment: "production",
+    accessToken: "c25695d18a2e4454ae98432b59249a0e",
+    environment: "testenv",
   };
   return (
     <Provider config={rollbarConfig}>
-      <AuthorizeProvider>
-        <BrowserRouter>
-          <div className="d-flex flex-column h-100">
-            <Navbar />
-            <Routes>
-              <Route path="login" element={<Login />} />
-              <Route path="signup" element={<SignUp />} />
-              <Route
-                path="/"
-                element={
-                  <RequireAuth>
-                    <Main />
-                  </RequireAuth>
-                }
-              />
-              <Route path="*" element={<Notfound />} />
-            </Routes>
-          </div>
-        </BrowserRouter>
-      </AuthorizeProvider>
+      <ErrorBoundary>
+        <AuthorizeProvider>
+          <BrowserRouter>
+            <div className="d-flex flex-column h-100">
+              <Navbar />
+              <Routes>
+                <Route path="login" element={<Login />} />
+                <Route path="signup" element={<SignUp />} />
+                <Route
+                  path="/"
+                  element={
+                    <RequireAuth>
+                      <Main />
+                    </RequireAuth>
+                  }
+                />
+                <Route path="*" element={<Notfound />} />
+              </Routes>
+            </div>
+          </BrowserRouter>
+        </AuthorizeProvider>
+      </ErrorBoundary>
     </Provider>
   );
 };
